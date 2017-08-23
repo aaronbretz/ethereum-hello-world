b792e858403d7eeeb504dcc0cf9ffe75a64c3926
abretz47 <abretz47@gmail.com>
2017-08-22 21:33:59 -0500

    Add files via upload

---
+++ ERC20.sol
@@ -0,0 +1,81 @@
+pragma solidity ^0.4.8;
+contract tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData); }
+
+contract BPThreewards {
+    /* Public variables of the token */
+    string public constant name = 'BPThreewards';
+    string public constant symbol = '%';
+    uint8 public constant decimals = 0;
+    uint256 public totalSupply;
+
+    /* This creates an array with all balances */
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    /* This generates a public event on the blockchain that will notify clients */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    event Approval(address indexed _owner, address indexed _spender, uint _value);
+
+    /* Initializes contract with initial supply tokens to the creator of the contract */
+    function BPThreewards(
+        uint256 initialSupply;
+    ) {
+        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
+        totalSupply = initialSupply;                        // Update total supply                      // Amount of decimals for display purposes
+    }
+
+    function () payable{}
+
+    function totalSupply() constant returns(uint supply){
+        return totalSupply;
+    }
+    function balanceOf(address _owner) returns (uint balance){
+        return balanceOf[_owner];
+    }
+    /* Send coins */
+    function transfer(address _to, uint256 _value) returns (bool success) {
+        if (_to == 0x0) throw;                               // Prevent transfer to 0x0 address. Use burn() instead
+        if (balanceOf[msg.sender] < _value) throw;           // Check if the sender has enough
+        if (balanceOf[_to] + _value < balanceOf[_to]) throw; // Check for overflows
+        balanceOf[msg.sender] -= _value;                     // Subtract from the sender
+        balanceOf[_to] += _value;                            // Add the same to the recipient
+        Transfer(msg.sender, _to, _value);                   // Notify anyone listening that this transfer took place
+        return true;
+    }
+
+    /* Allow another contract to spend some tokens in your behalf */
+    function approve(address _spender, uint256 _value)
+    returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        Approval(msg.sender,_spender,_value);
+        return true;
+    }
+
+    /* Approve and then communicate the approved contract in a single tx */
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
+    returns (bool success) {
+        tokenRecipient spender = tokenRecipient(_spender);
+        if (approve(_spender, _value)) {
+            spender.receiveApproval(msg.sender, _value, this, _extraData);
+            return true;
+        }
+    }
+
+    /* A contract attempts to get the coins */
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+        if (_to == 0x0) throw;                                // Prevent transfer to 0x0 address. Use burn() instead
+        if (balanceOf[_from] < _value) throw;                 // Check if the sender has enough
+        if (balanceOf[_to] + _value < balanceOf[_to]) throw;  // Check for overflows
+        if (_value > allowance[_from][msg.sender]) throw;     // Check allowance
+        balanceOf[_from] -= _value;                           // Subtract from the sender
+        balanceOf[_to] += _value;                             // Add the same to the recipient
+        allowance[_from][msg.sender] -= _value;
+        Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) constant returns(uint remaining){
+        return allowance[_owner][_spender];
+    }
+}
